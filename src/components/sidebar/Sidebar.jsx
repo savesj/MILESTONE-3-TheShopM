129d4eaa773fdecfdd7f32987d8336d59804c5c0
savesj <jesuslares.20@hotmail.com>
2023-01-14 10:21:09 -0500

    Initializing Material UI for Icons and Inserting Icons

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,29 +1,72 @@
 import "./sidebar.scss"
+import LineAxisRoundedIcon from '@mui/icons-material/LineAxisRounded';
+import PeopleOutlineRoundedIcon from '@mui/icons-material/PeopleOutlineRounded';
+import MoreTimeRoundedIcon from '@mui/icons-material/MoreTimeRounded';
+import InventoryRoundedIcon from '@mui/icons-material/InventoryRounded';
+import CalendarMonthOutlinedIcon from '@mui/icons-material/CalendarMonthOutlined';
+import HealthAndSafetyOutlinedIcon from '@mui/icons-material/HealthAndSafetyOutlined';
+import SmsOutlinedIcon from '@mui/icons-material/SmsOutlined';
+import AssessmentOutlinedIcon from '@mui/icons-material/AssessmentOutlined';
+import PaidOutlinedIcon from '@mui/icons-material/PaidOutlined';
+import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
+import LogoutOutlinedIcon from '@mui/icons-material/LogoutOutlined';
 
 const Sidebar= () => {
     return (
         <div className="sidebar"> 
-            <div className="top"><span className="logo">The Manager  </span>
+            <div className="top"><span className="logo">The Shop Manager   </span>
             </div>
+            <hr  />
             <div className="center">
                 <ul>
+                    <p className="title">Main</p>
                     <li>
+                        <LineAxisRoundedIcon className="icon"/>
                         <span>Dashboard</span>
                     </li>
+                    <p className="title">List</p>
                     <li>
-                        <span>Dashboard</span>
+                        <PeopleOutlineRoundedIcon className="icon"/>
+                        <span>Costumers</span>
                     </li>
                     <li>
-                        <span>Dashboard</span>
+                        <MoreTimeRoundedIcon className="icon"/>
+                        <span>Work Board</span>
                     </li>
                     <li>
-                        <span>Dashboard</span>
+                        <InventoryRoundedIcon className="icon"/>
+                        <span>Inventory</span>
+                    </li>
+                    <p className="title">Useful</p>
+                    <li>
+                        <CalendarMonthOutlinedIcon className="icon"/>
+                        <span>Scheldule</span>
                     </li>
                     <li>
-                        <span>Dashboard</span>
+                        <HealthAndSafetyOutlinedIcon className="icon"/>
+                        <span>Inspection</span>
                     </li>
                     <li>
-                        <span>Dashboard</span>
+                        <SmsOutlinedIcon className="icon"/>
+                        <span>Messages</span>
+                    </li>
+                    <li>
+                        <AssessmentOutlinedIcon className="icon"/>
+                        <span>Reports</span>
+                    </li>
+                    <p className="title">Service</p>
+                    <li>
+                        <PaidOutlinedIcon className="icon"/>
+                        <span>Payment</span>
+                    </li>
+                    <p className="title">User</p>
+                     <li>
+                        <AccountCircleOutlinedIcon className="icon"/>
+                        <span>Profile</span>
+                    </li>
+                    <li>
+                        <LogoutOutlinedIcon className="icon"/>
+                        <span>Logout</span>
                     </li>
                 </ul>
             </div>
