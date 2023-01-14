dddfde3a2fd56783d3c0bff5b2e4cfafcd6c1718
savesj <jesuslares.20@hotmail.com>
2023-01-14 18:13:45 -0500

    Finished Navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,15 +1,27 @@
 import "./navbar.scss"
 import ManageSearchRoundedIcon from '@mui/icons-material/ManageSearchRounded';
-
+import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
+import NotificationsOutlinedIcon from '@mui/icons-material/NotificationsOutlined';
+import AvatarI from './avatar.png'
 const Navbar= () => {
     return (
         <div className="navbar"> 
             <div className="wrapper">
                 <div className="search">
                     <input type="text" placeholder="Search Costumer's Name" />
-                    <ManageSearchRoundedIcon/>
+                    <ManageSearchRoundedIcon className="lookup"/>
                 </div>
-
+                <div className="items">
+                    <div className="item">
+                        <DarkModeOutlinedIcon/>
+                    </div>
+                    <div className="item">    
+                        <NotificationsOutlinedIcon/>
+                    </div>
+                    <div className="item">    
+                    <img src={AvatarI} className="avatar"/>
+                    </div>
+                </div>
             </div>
         </div>
     );
